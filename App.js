6c471dcd471fcd497002281f4615d8322c946db3
adhxabre <abeldustin06@gmail.com>
2023-04-19 10:52:08 +0700

    Styling

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import State from "./src/screens/state";
+import Form from "./src/screens/form";
 
 export default function App() {
   return (
     <View style={{ paddingTop: 100 }}>
       <StatusBar style="auto" />
-      <State />
+      <Form />
     </View>
   );
 }

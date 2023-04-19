fbdfb558b7d5c7fd0a9ffc0d47e81e777cf421c0
adhxabre <abeldustin06@gmail.com>
2023-04-19 11:02:20 +0700

    FlatList

--- src/screens/map.js
+++ src/screens/map.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, FlatList, Text } from "react-native";
 
 export default function Map() {
-  const cars = ["BMW", "Mercedez", "Ferarri", "Lexus"];
-
   return (
     <View>
       <Text>Mapping List of Cars</Text>
-      {cars.map((car) => (
-        <Text key={car}>{car}</Text>
-      ))}
+      <FlatList
+        data={[
+          { name: "Abel Dustin" },
+          { name: "Ilham Fathullah" },
+          { name: "Cintara Surya" },
+        ]}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 }

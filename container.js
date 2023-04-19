16b6c0a3c1be8bea70736c41dcb9951f83d83bc7
adhxabre <abeldustin06@gmail.com>
2023-04-19 13:37:43 +0700

    Stack navigator

--- container.js
+++ container.js
@@ -1,8 +1,52 @@
 import React from "react";
 
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { useTheme } from "native-base";
+
 import FormNativeBase from "./src/screens/form";
 import Hello from "./src/screens/hello";
+import IncDec from "./src/screens/incDec";
+
+const Stack = createStackNavigator();
 
 export default function Container() {
-  return <FormNativeBase />;
+  const theme = useTheme();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerMode: "screen",
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: theme.colors.muted["800"] },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Hello}
+          options={{
+            title: "Hello Screen",
+          }}
+        />
+        <Stack.Screen
+          name="IncDec"
+          component={IncDec}
+          options={{
+            title: "Increment Decrement",
+          }}
+        />
+        <Stack.Screen
+          name="Form"
+          component={FormNativeBase}
+          options={{
+            title: "Sign In",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/screens/hello.js
+++ src/screens/hello.js
@@ -1,12 +1,41 @@
 import * as React from "react";
-import { Text, Box } from "native-base";
+import { Text, Box, Pressable } from "native-base";
 
-export default function Hello() {
+export default function Hello({ navigation }) {
   return (
     <Box bg="primary.400" flex={1} alignItems="center" justifyContent="center">
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
         Life's too short
       </Text>
+
+      <Pressable
+        onPress={() => navigation.navigate("IncDec")}
+        style={{
+          backgroundColor: "#487eb0",
+          height: 40,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 10,
+          margin: 20,
+        }}
+      >
+        <Text>Screen Increment and Decrement</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate("Form")}
+        style={{
+          backgroundColor: "#487eb0",
+          height: 40,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 10,
+          margin: 20,
+        }}
+      >
+        <Text>Sign In</Text>
+      </Pressable>
     </Box>
   );
 }
